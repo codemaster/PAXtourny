d9af6baff208e248f7facfd04a6e2350a06418aa
Andrew Kane <codemaster@quantumraiders.com>
2011-12-03 16:18:34 -0800

    - Updated .gitignore - Added tournament application

--- PAXtourny/settings.py
+++ PAXtourny/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Andrew Kane', 'andrew@andrewmkane.com')
 )
 
 MANAGERS = ADMINS
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Codemaster\\Documents\\Programming\\PAXtourny\\sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': 'tourny',                      # Not used with sqlite3.
-        'PASSWORD': 'derp',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': './db/sqlite.db',               # Or path to database file if using sqlite3.
+        'USER': '',                             # Not used with sqlite3.
+        'PASSWORD': '',                         # Not used with sqlite3.
+        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
+     'tournament'
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ PAXtourny/tournament/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Tournament(models.Model):
+    name = models.CharField("Name of the tournament", max_length=255)
+    team_size = models.PositiveSmallIntegerField("Size of the teams for this tournament")
+    teams_per_match = models.PositiveSmallIntegerField("Number of teams that will be in each match")
+    
+class Player(models.Model):
+    name = models.CharField("Player's name", max_length=255)
+    handle = models.CharField("Player's handle/nickname", max_length=255)
+    email = models.EmailField("Player's e-mail address", unique=True)
+    
+class Team(models.Model):
+    name = models.CharField("The name of the teams", max_length=255)
+    members = models.ManyToManyField(Player, verbose_name="The players on the team")
+    
+class Match(models.Model):
+    tournament = models.ForeignKey(Tournament, verbose_name="The tournament that this match belongs to")
+    parent = models.ForeignKey("Match", verbose_name="The optional parent match")
+    teams = models.ManyToManyField(Team, verbose_name="The teams that are in this match")
