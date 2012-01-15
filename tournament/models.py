ae51edcf6346d5bd269a57b4c4ca2044c045817e
Andrew Kane <codemaster@quantumraiders.com>
2012-01-14 22:56:57 -0800

    Added additional relational connections for Teams Added additional match information (winner, start time)

--- tournament/models.py
+++ tournament/models.py
@@ -19,6 +19,7 @@ class Player(models.Model):
 class Team(models.Model):
     name = models.CharField("The name of the teams", max_length=255)
     members = models.ManyToManyField(Player, verbose_name="The players on the team")
+    tournaments = models.ManyToManyField(Tournament, verbose_name="The tournaments that this team has signed up for", null=True)
     
     def __unicode__(self):
         return self.name
@@ -27,6 +28,8 @@ class Match(models.Model):
     tournament = models.ForeignKey(Tournament, verbose_name="The tournament that this match belongs to")
     parent = models.ForeignKey("Match", verbose_name="The optional parent match", blank=True, null=True)
     teams = models.ManyToManyField(Team, verbose_name="The teams that are in this match")
+    when = models.DateTimeField("When the match is due to start", null=True)
+    winner = models.ForeignKey(Team, verbose_name="The team that won",related_name='wining_team')
     
     def __unicode__(self):
         return " vs ".join([team.name for team in self.teams.all()])
