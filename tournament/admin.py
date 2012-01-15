206cb0662defcc00e05422a390ed30fee59f11da
Andrew Kane <codemaster@quantumraiders.com>
2012-01-14 22:57:51 -0800

    Added automatic creation of 1-member teams for players

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,7 +1,19 @@
-from tournament.models import Tournament, Player, Match, Team
+from models import Tournament, Player, Match, Team
 from django.contrib import admin
 
+class PlayerAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change:
+            team = Team()
+            team.name = obj.handle + " (Team)"
+            team.save()
+            obj.save()
+            team.members.add(obj)
+            team.save()
+        else:
+            obj.save()
+            
 admin.site.register(Tournament)
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(Match)
 admin.site.register(Team)
